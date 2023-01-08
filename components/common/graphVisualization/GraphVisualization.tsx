bfb0b73d81918ccdae97546cad323a1810ad2a75
moonstar-x <christian.lopez99@outlook.com>
2023-01-08 05:53:26 -0500

    Created a preliminary graph visualizer.

---
+++ components/common/graphVisualization/GraphVisualization.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from 'react';
+import type { Node, Edge } from './types';
+import { Network, Data, Options } from 'vis-network/esnext/umd/vis-network.min';
+import { DataSet } from 'vis-data/esnext/umd/vis-data.min';
+
+interface Props {
+  nodes: Node[]
+  edges: Edge[]
+
+  width?: number | string
+  height?: number | string
+}
+
+const GraphVisualization: React.FC<Props> = ({ nodes, edges, width = 600, height = 400 }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const data: Data = {
+      nodes: new DataSet(nodes),
+      edges: new DataSet(edges)
+    };
+    const options: Options = {
+      nodes: {
+        shape: 'square'
+      }
+    };
+
+    const network = new Network(containerRef.current!, data, options);
+
+    return () => {
+      network.destroy();
+    };
+  }, [nodes, edges]);
+
+  return (
+    <div ref={containerRef} style={{ width, height }} />
+  );
+};
+
+export default GraphVisualization;
---
+++ components/common/graphVisualization/types.ts
@@ -0,0 +1,10 @@
+export interface Node {
+  id: string
+  label: string
+}
+
+export interface Edge {
+  from: string
+  to: string
+  id: string
+}
