e40c11ab4560804e2c30bf9db19e7f81692aac5a
moonstar-x <christian.lopez99@outlook.com>
2023-01-08 05:53:37 -0500

    Added an example visualization in index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { GetServerSidePropsResult } from 'next';
 import { PageMetadata } from '@components/common/pageMetadata';
 import { PageWrapper } from '@components/common/pageWrapper';
+import { GraphVisualization } from '@components/common/graphVisualization';
 import { getPeptidesConstitutedBy } from '@lib/services/graphDb/peptideService';
 
 interface ServerSideProps {
@@ -13,13 +14,28 @@ interface Props extends ServerSideProps {
 }
 
 const HomePage: React.FC<Props> = ({ result }) => {
-  console.log(result);
+  const { nodes, edges } = result.reduce((acc: any, r: any) => {
+    for (const item of r) {
+      acc.edges.push({ id: null, from: item.start, to: item.end });
+
+      if (!acc.nodes.find((n: any) => n.id === item.start)) {
+        acc.nodes.push({ id: item.start, label: item.startName });
+      }
+      if (!acc.nodes.find((n: any) => n.id === item.end)) {
+        acc.nodes.push({ id: item.end, label: item.endName });
+      }
+    }
+
+    return acc;
+  }, { nodes: [], edges: [] });
 
   return (
     <PageWrapper>
       <PageMetadata title="Home" />
 
       Main Page
+
+      <GraphVisualization nodes={nodes} edges={edges} />
     </PageWrapper>
   );
 };
