fbf03268b1ebc5de8d8c52dd90fffbbe90c68f98
moonstar-x <christian.lopez99@outlook.com>
2023-01-10 20:57:58 -0500

    Added a peptide page.

--- lib/services/graphDb/peptideService.ts
+++ lib/services/graphDb/peptideService.ts
@@ -1,6 +1,8 @@
 import { readTransaction } from './dbService';
 import type { PathSegment } from 'neo4j-driver';
 
+// eslint-disable-next-line no-warning-comments
+// TODO: Remove this function.
 export const getPeptidesConstitutedBy = async () => {
   const query = 'MATCH p=()-[r:constituted_by]->() RETURN p LIMIT 25';
   const result = await readTransaction(query);
@@ -16,4 +18,24 @@ export const getPeptidesConstitutedBy = async () => {
       };
     });
   });
+};
+
+// eslint-disable-next-line no-warning-comments
+// TODO: Get Peptide with properties.
+export const getPeptideBySeq = async (seq: string): Promise<string | null> => {
+  const query = 'MATCH (n:Peptide {seq: $seq}) RETURN n';
+  const result = await readTransaction(query, { seq });
+
+  if (result.records.length < 1) {
+    return null;
+  }
+
+  return result.records[0].get('n').properties.seq;
+};
+
+export const searchPeptidesSingleQuery = async (seq: string, limit: number, skip: number): Promise<string[]> => {
+  const query = 'MATCH (n:Peptide) WHERE n.seq CONTAINS $seq RETURN n SKIP $skip LIMIT $limit';
+  const result = await readTransaction(query, { seq, limit, skip });
+
+  return result.records.map((r) => r.get('n'));
 };
---
+++ pages/peptide/[seq].tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import type { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import { PageMetadata } from '@components/common/pageMetadata';
+import { PageWrapper } from '@components/common/pageWrapper';
+import { getPeptideBySeq } from '@lib/services/graphDb/peptideService';
+
+interface ServerSideProps {
+  sequence: string
+  peptide: string
+}
+
+interface Props extends ServerSideProps {
+
+}
+
+const PeptidePage: React.FC<Props> = ({ sequence, peptide }) => {
+  return (
+    <PageWrapper>
+      <PageMetadata title={sequence} />
+
+      Peptide Page for {sequence}
+
+      <pre>
+        {JSON.stringify(peptide, null, 2)}
+      </pre>
+    </PageWrapper>
+  );
+};
+
+export const getServerSideProps = async (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<ServerSideProps>> => {
+  const seq = context.params!.seq as string;
+  const peptide = await getPeptideBySeq(seq);
+
+  if (!peptide) {
+    return {
+      notFound: true
+    };
+  }
+
+  return {
+    props: {
+      sequence: seq,
+      peptide: peptide
+    }
+  };
+};
+
+export default PeptidePage;
