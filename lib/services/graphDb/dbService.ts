0c45f0dab7b678d2bc140c415080cda5338a85eb
moonstar-x <christian.lopez99@outlook.com>
2023-01-12 20:26:15 -0500

    Created a getNodeCount service function.

--- lib/services/graphDb/dbService.ts
+++ lib/services/graphDb/dbService.ts
@@ -1,6 +1,9 @@
 import neo4j from 'neo4j-driver';
 import { BUILDING_STAGE, NEO4J_DB_URI } from '@lib/config';
 
+export const NODE_LABELS = ['Origin', 'Target', 'Peptide', 'CrossRef', 'Database', 'Function', 'Cterminus', 'Nterminus', 'UnusualAA'] as const;
+export type NodeLabel = typeof NODE_LABELS[number];
+
 // Don't start a connection while building.
 export const driver = (BUILDING_STAGE !== 'true' ? neo4j.driver(NEO4J_DB_URI) : null)!;
 
@@ -22,4 +25,19 @@ export const readTransaction = async (query: string, params?: Record<string, str
 
   await session.close();
   return result;
+};
+
+export const getNodeCount = async (nodeLabel: NodeLabel): Promise<number> => {
+  if (!NODE_LABELS.includes(nodeLabel)) {
+    throw new TypeError('Invalid node label supplied to getNodeCount.');
+  }
+
+  const session = getSession();
+  const result = await session.readTransaction((tx) => {
+    const query = `MATCH (n:${nodeLabel}) RETURN COUNT(n) AS n`; // We can afford to inject the nodeLabel directly because a value check is done previously.
+    return tx.run(query, { nodeLabel });
+  });
+
+  await session.close();
+  return result.records[0]?.get('n').toInt() ?? 0;
 };
