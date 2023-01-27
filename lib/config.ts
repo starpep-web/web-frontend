e4228d7a2ae2064e83a235ba3ab347850042df71
moonstar-x <christian.lopez99@outlook.com>
2023-01-26 19:05:25 -0500

    Fixed env variables not usable in React components.

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
-import 'dotenv/config';
+export const BUILDING_STAGE = process.env.NEXT_APP_BUILDING_STAGE as string;
 
-export const BUILDING_STAGE = process.env.BUILDING_STAGE as string;
+export const DATAMINING_API_URL = process.env.NEXT_APP_DATAMINING_API_URL as string;
+export const NEO4J_DB_URI = process.env.NEXT_APP_NEO4J_DB_URI as string;
 
-export const DATAMINING_API_URL = process.env.DATAMINING_API_URL as string;
-export const NEO4J_DB_URI = process.env.NEO4J_DB_URI as string;
+export const WEBSITE_URL = process.env.NEXT_APP_WEBSITE_URL as string;
