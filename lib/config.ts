ad1e95c02afa8461b236e4b92aeac62bf16430bc
moonstar-x <christian.lopez99@outlook.com>
2023-01-26 21:50:31 -0500

    Fixed bad env variables in build.

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
-export const BUILDING_STAGE = process.env.NEXT_APP_BUILDING_STAGE as string;
+export const NODE_ENV = process.env.NODE_ENV as string;
+export const BUILDING_STAGE = process.env.BUILDING_STAGE as string;
 
-export const DATAMINING_API_URL = process.env.NEXT_APP_DATAMINING_API_URL as string;
-export const NEO4J_DB_URI = process.env.NEXT_APP_NEO4J_DB_URI as string;
+export const DATAMINING_API_URL = process.env.DATAMINING_API_URL as string;
+export const NEO4J_DB_URI = process.env.NEO4J_DB_URI as string;
 
-export const WEBSITE_URL = process.env.NEXT_APP_WEBSITE_URL as string;
+export const WEBSITE_URL = NODE_ENV === 'production' ? 'https://webpep.moonstar-x.dev' : 'http://localhost:3000';
--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 require('dotenv').config();
 
-const filteredEnvEntries = Object.entries(process.env).filter(([k]) => k.startsWith('NEXT_APP_'));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  env: Object.fromEntries(filteredEnvEntries)
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
