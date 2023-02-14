e4570fa2f6752158fab743c00afcea235f30233c
moonstar-x <christian.lopez99@outlook.com>
2023-02-14 00:43:01 -0500

    Removed title page graph.

--- lib/services/graphDb/peptideService.ts
+++ lib/services/graphDb/peptideService.ts
@@ -1,32 +1,12 @@
 import { readTransaction } from './dbService';
-import type { PathSegment } from 'neo4j-driver';
 import {
   Peptide,
   FullPeptide,
   PeptideMetadata,
   RawRelationshipLabel,
-  getRelationshipLabelFromRaw,
+  getRelationshipLabelFromRaw
 } from '@lib/models/peptide';
 import { WithPagination, createPagination } from '@lib/utils/pagination';
-
-// eslint-disable-next-line no-warning-comments
-// TODO: Remove this function.
-export const getPeptidesConstitutedBy = async () => {
-  const query = 'MATCH p=()-[r:constituted_by]->() RETURN p LIMIT 25';
-  const result = await readTransaction(query);
-
-  return result.records.map((r) => {
-    return r.get('p').segments.map((s: PathSegment<any>) => {
-      return {
-        start: s.start.identity.toString(),
-        startName: s.start.properties.seq,
-        end: s.end.identity.toString(),
-        endName: s.end.properties.name,
-        relationshipType: s.relationship.type
-      };
-    });
-  });
-};
 
 export const getPeptideBySequence = async (sequence: string): Promise<FullPeptide | null> => {
   const query = 'MATCH (n:Peptide {seq: $sequence})-[r]->(v) RETURN n, r, v';
