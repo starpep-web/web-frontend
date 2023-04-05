59b481fa909cf264ec32dd5cfa86a4de83e5763f
moonstar-x <christian.lopez99@outlook.com>
2023-04-05 01:39:31 -0500

    Target distribution is now shown in statistics page.

--- pages/statistics/index.tsx
+++ pages/statistics/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GetServerSidePropsResult } from 'next';
+import { Columns } from 'react-bulma-components';
 import { PageMetadata } from '@components/common/pageMetadata';
 import { PageWrapper } from '@components/common/pageWrapper';
 import { NumberStatistic } from '@components/statistics/numberStatistic';
@@ -17,6 +18,8 @@ interface Props extends ServerSideProps {
 }
 
 const StatisticsPage: React.FC<Props> = ({ statistics }) => {
+  const graphHeight = 400;
+
   return (
     <PageWrapper>
       <PageMetadata title="Statistics" />
@@ -24,16 +27,27 @@ const StatisticsPage: React.FC<Props> = ({ statistics }) => {
       <NumberStatistic title="1. Total Peptides" value={statistics.count} />
       <NumberStatistic title="2. Total Peptides with Unusual AA's" value={statistics.unusualCount} />
 
-      <WithTitledBox title="3. Peptide Distribution by Sequence Length" height={400}>
+      <WithTitledBox title="3. Peptide Distribution by Sequence Length" height={graphHeight}>
         <BarChart id="length-distribution" data={statistics.lengthDistribution} yTitle="Frequency" xTitle="Sequence Length" />
       </WithTitledBox>
 
-      <WithTitledBox title="4. Peptide Distribution by Function" height={400}>
+      <WithTitledBox title="4. Peptide Distribution by Function" height={graphHeight}>
         <BarChart id="function-distribution" data={statistics.functionDistribution} />
       </WithTitledBox>
 
-      <WithTitledBox title="5. Peptide Distribution by Database" height={400}>
+      <WithTitledBox title="5. Peptide Distribution by Database" height={graphHeight}>
         <BarChart id="database-distribution" data={statistics.databaseDistribution} />
+      </WithTitledBox>
+
+      <WithTitledBox title={`6. Top ${statistics.targetDistribution.partialSize} Peptide Distribution by Target`}>
+        <Columns>
+          <Columns.Column style={{ height: graphHeight }} tablet={{ size: 12 }} desktop={{ size: 6 }}>
+            <BarChart id="target-distribution" data={statistics.targetDistribution.distribution} />
+          </Columns.Column>
+          <Columns.Column style={{ height: graphHeight }} tablet={{ size: 12 }} desktop={{ size: 6 }}>
+            <BarChart id="target-percentage" data={statistics.targetDistribution.percentage} />
+          </Columns.Column>
+        </Columns>
       </WithTitledBox>
     </PageWrapper>
   );
