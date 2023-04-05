8eb932c9749bcf53466cd280fc96c0185b6ea25a
moonstar-x <christian.lopez99@outlook.com>
2023-04-05 00:47:29 -0500

    Created partial statistics for target.

--- lib/models/statistics.ts
+++ lib/models/statistics.ts
@@ -1,7 +1,15 @@
+export interface PartialRelationStatistics {
+  distribution: Record<string, number>,
+  percentage: Record<string, number>,
+  total: number,
+  partialSize: number
+}
+
 export interface DatabaseStatistics {
   count: number,
   unusualCount: number,
   lengthDistribution: Record<number, number>,
   functionDistribution: Record<string, number>,
-  databaseDistribution: Record<string, number>
+  databaseDistribution: Record<string, number>,
+  targetDistribution: PartialRelationStatistics
 }
--- lib/services/graphDb/statisticsService.ts
+++ lib/services/graphDb/statisticsService.ts
@@ -1,5 +1,5 @@
 import { readTransaction } from './dbService';
-import { DatabaseStatistics } from '@lib/models/statistics';
+import { DatabaseStatistics, PartialRelationStatistics } from '@lib/models/statistics';
 
 export const getPeptideCount = async (): Promise<number> => {
   const query = 'MATCH (n:Peptide) RETURN COUNT(n) AS c';
@@ -51,12 +51,48 @@ export const getPeptideDatabaseDistribution = async (): Promise<Record<string, n
   }));
 };
 
-export const getDatabaseStatistics = async (): Promise<DatabaseStatistics> => {
+export const getPartialPeptideTargetDistribution = async (limit: number): Promise<PartialRelationStatistics> => {
+  const query = `
+MATCH (n:Peptide)-[r:assessed_against]->(v)
+WITH v.name AS target, COUNT(*) AS frequency
+WITH COLLECT({\`target\`: target, \`freq\`: frequency}) as aggregate, SUM(frequency) AS total
+UNWIND aggregate AS agg
+WITH agg.target AS target, agg.freq AS frequency, total
+RETURN target, frequency, total, toFloat(frequency) / total AS percentage
+ORDER BY frequency DESC
+LIMIT $limit
+  `;
+  const result = await readTransaction(query, { limit });
+
+  const distribution = Object.fromEntries(result.records.map((record) => {
+    const target = record.get('target');
+    const frequency = record.get('frequency').toInt();
+
+    return [target, frequency];
+  }));
+  const percentage = Object.fromEntries(result.records.map((record) => {
+    const target = record.get('target');
+    const percentage = record.get('percentage');
+
+    return [target, percentage];
+  }));
+  const total = result.records[0]?.get('total').toInt() ?? 0;
+
+  return {
+    distribution,
+    percentage,
+    total,
+    partialSize: limit
+  };
+};
+
+export const getDatabaseStatistics = async (partialsLimit = 50): Promise<DatabaseStatistics> => {
   return {
     count: await getPeptideCount(),
     unusualCount: await getUnusualCount(),
     lengthDistribution: await getPeptideLengthDistribution(),
     functionDistribution: await getPeptideFunctionDistribution(),
-    databaseDistribution: await getPeptideDatabaseDistribution()
+    databaseDistribution: await getPeptideDatabaseDistribution(),
+    targetDistribution: await getPartialPeptideTargetDistribution(partialsLimit)
   };
 };
