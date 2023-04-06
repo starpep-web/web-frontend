5f3538b4f4307311bdd6ec29e7e9c9fa8f19448d
moonstar-x <christian.lopez99@outlook.com>
2023-04-06 14:25:52 -0500

    Added CominSoonPlaceholder to *query search boxes.

--- components/search/peptideSearchBox/MultiQueryPeptideSearchBox.tsx
+++ components/search/peptideSearchBox/MultiQueryPeptideSearchBox.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import { ComingSoonPlaceholder } from '@components/common/comingSoon';
 
 const MultiQueryPeptideSearchBox = () => {
   return (
-    <div>
-      Coming soon...
-    </div>
+    <ComingSoonPlaceholder />
   );
 };
 
