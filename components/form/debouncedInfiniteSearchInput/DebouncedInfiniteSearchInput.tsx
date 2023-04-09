a27fb8d63c1694ceb1147736a976fa4c719812f4
moonstar-x <christian.lopez99@outlook.com>
2023-04-08 22:27:00 -0500

    DebouncedSearchInput now receives an initialValue.

--- components/form/debouncedInfiniteSearchInput/DebouncedInfiniteSearchInput.tsx
+++ components/form/debouncedInfiniteSearchInput/DebouncedInfiniteSearchInput.tsx
@@ -11,12 +11,13 @@ interface Props {
 
   label?: string
   placeholder?: string
+  initialValue?: string
   icon?: IconProp
 }
 
-const DebouncedInfiniteSearchInput: React.FC<Props> = ({ dataFetch, onChange, label, placeholder, icon }) => {
+const DebouncedInfiniteSearchInput: React.FC<Props> = ({ dataFetch, onChange, label, placeholder, initialValue, icon }) => {
   const [open, setOpen] = useState<boolean>(false);
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>(initialValue ?? '');
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [loading, setLoading] = useState<boolean>(false);
   const [allOptions, setAllOptions] = useState<string[]>([]);
