eae9e8b6afe862342d65e1d88eac8cbb33e17251
moonstar-x <christian.lopez99@outlook.com>
2023-04-10 15:21:08 -0500

    Added type as a paremeter to frequency service.

--- lib/services/graphDb/statisticsService.ts
+++ lib/services/graphDb/statisticsService.ts
@@ -2,6 +2,7 @@ import { Integer } from 'neo4j-driver';
 import { readTransaction } from './dbService';
 import { DatabaseStatistics, PartialRelationStatistics } from '@lib/models/statistics';
 import { createAlphabet } from '@lib/utils/array';
+import { BadRequestError } from '@lib/errors/http';
 
 export const getPeptideCount = async (): Promise<number> => {
   const query = 'MATCH (n:Peptide) RETURN COUNT(n) AS c';
@@ -207,7 +208,8 @@ export const getDatabaseStatistics = async (partialsLimit = 25): Promise<Databas
   };
 };
 
-export type FrequencyFilterType = 'Database' | 'Function' | 'Origin';
+export const FREQUENCY_FILTER_TYPES = ['Database', 'Function', 'Origin'] as const;
+export type FrequencyFilterType = typeof FREQUENCY_FILTER_TYPES[number];
 
 export const getTotalAAFrequency = async (): Promise<Record<string, number>> => {
   const query = "MATCH (n:Peptide) WITH apoc.text.join(COLLECT(n.seq), '') AS seqText WITH apoc.coll.frequenciesAsMap(SPLIT(seqText, '')) AS freq RETURN freq";
@@ -219,8 +221,13 @@ export const getTotalAAFrequency = async (): Promise<Record<string, number>> => 
   }));
 };
 
-export const getFilterAAFrequency = async (filter: string): Promise<Record<string, number>> => {
-  const query = "MATCH (n:Peptide)-[]-(v) WHERE v.name = $filter WITH apoc.text.join(COLLECT(n.seq), '') AS seqText WITH apoc.coll.frequenciesAsMap(SPLIT(seqText, '')) AS freq RETURN freq";
+export const getFilterAAFrequency = async (type: FrequencyFilterType, filter: string): Promise<Record<string, number>> => {
+  if (!FREQUENCY_FILTER_TYPES.includes(type)) {
+    throw new BadRequestError(`Invalid filter type ${type} supplied, must be one of ${FREQUENCY_FILTER_TYPES.join(', ')}`);
+  }
+
+  // We can interpolate the type into the query because we're checking above it is valid.
+  const query = `MATCH (n:Peptide)-[]-(v: ${type}) WHERE v.name = $filter WITH apoc.text.join(COLLECT(n.seq), '') AS seqText WITH apoc.coll.frequenciesAsMap(SPLIT(seqText, '')) AS freq RETURN freq`;
   const result = await readTransaction(query, { filter });
   const resultObject: Record<string, Integer> = result.records[0]?.get('freq') ?? {};
 
--- pages/api/statistics/amino-acids/frequency.ts
+++ pages/api/statistics/amino-acids/frequency.ts
@@ -1,16 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import HttpStatus from 'http-status-codes';
 import { ResponseBuilder, ResponseBody } from '@lib/utils/http';
-import { NODE_LABELS } from '@lib/models/peptide';
-import { FrequencyFilterType, getFilterAAFrequency, getTotalAAFrequency } from '@lib/services/graphDb/statisticsService';
+import { FREQUENCY_FILTER_TYPES, FrequencyFilterType, getFilterAAFrequency, getTotalAAFrequency } from '@lib/services/graphDb/statisticsService';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseBody>) => {
   const type = Array.isArray(req.query.type) ? req.query.type[0] : req.query.type ?? '';
   const value = Array.isArray(req.query.value) ? req.query.value[0] : req.query.value ?? '';
 
-  const isFilterValid = value && NODE_LABELS.includes(type as FrequencyFilterType);
+  const castType = type as FrequencyFilterType;
+  const isFilterValid = value && FREQUENCY_FILTER_TYPES.includes(castType);
 
-  const frequency = isFilterValid ? await getFilterAAFrequency(value) : await getTotalAAFrequency();
+  const frequency = isFilterValid ? await getFilterAAFrequency(castType, value) : await getTotalAAFrequency();
 
   const response = new ResponseBuilder()
     .withStatusCode(HttpStatus.OK)
