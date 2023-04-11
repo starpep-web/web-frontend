fc640938c40f9100de4a812ff28f67438e2e3113
moonstar-x <christian.lopez99@outlook.com>
2023-04-11 17:57:30 -0500

    Created a blank ControlsOverlay for PDBViewer.

---
+++ components/pdb/controlsOverlay/ControlsOverlay.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ControlsOverlay.module.scss';
+
+const ControlsOverlay = () => {
+  return (
+    <div className={styles.controlsOverlay}>
+      CONTROLS
+    </div>
+  );
+};
+
+export default ControlsOverlay;
