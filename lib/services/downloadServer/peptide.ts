f65003ac8e37d8bbae79fa652604d08b37624181
moonstar-x <christian.lopez99@outlook.com>
2023-04-10 23:54:34 -0500

    Added a getPeptidePdbContent function.

--- lib/services/downloadServer/peptide.ts
+++ lib/services/downloadServer/peptide.ts
@@ -1,5 +1,15 @@
+import axios from 'axios';
 import { DOWNLOAD_WEBSITE_URL } from '@lib/config';
 
 export const getPeptidePdbDownloadUrl = (starPepId: string) => {
   return `${DOWNLOAD_WEBSITE_URL}/pdb/${starPepId}.pdb`;
+};
+
+export const getPeptidePdbContent = async (starPepId: string): Promise<string> => {
+  try {
+    return (await axios.get(getPeptidePdbDownloadUrl(starPepId))).data;
+  } catch (error) {
+    console.error(error);
+    return '';
+  }
 };
