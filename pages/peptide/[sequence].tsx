94db3a0ccabfe78a3d775f1fb80bb3fc8c9a44cc
moonstar-x <christian.lopez99@outlook.com>
2023-04-11 17:57:58 -0500

    Added PeptideViewer to the PeptidePage.

--- pages/peptide/[sequence].tsx
+++ pages/peptide/[sequence].tsx
@@ -2,26 +2,31 @@ import React from 'react';
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import { PageMetadata } from '@components/common/pageMetadata';
 import { PageWrapper } from '@components/common/pageWrapper';
-import { PeptideGraph } from '@components/graphs/peptideGraph';
 import { PeptideTitle } from '@components/peptide/peptideTitle';
+import { PeptideGraph } from '@components/graphs/peptideGraph';
+import { PeptideViewer } from '@components/pdb/peptideViewer';
 import { PeptideInfo } from '@components/peptide/peptideInfo';
 import { PeptideDownloads } from '@components/peptide/peptideDownloads';
 import { PeptideMetadata } from '@components/peptide/peptideMetadata';
 import { getPeptideBySequence } from '@lib/services/graphDb/peptideService';
 import { FullPeptide } from '@lib/models/peptide';
+import { getPeptidePdbContent } from '@lib/services/downloadServer/peptide';
 
 interface ServerSideProps {
   peptide: FullPeptide
+  pdbString: string
 }
 
 interface Props extends ServerSideProps {
 
 }
 
-const PeptidePage: React.FC<Props> = ({ peptide }) => {
+const PeptidePage: React.FC<Props> = ({ peptide, pdbString }) => {
   return (
     <PageWrapper>
       <PageMetadata title={peptide.sequence} />
+
+      <PeptideViewer pdb={pdbString} width="100%" height="100vh" />
 
       <PeptideTitle sequence={peptide.sequence} />
       <PeptideGraph peptide={peptide} width="100%" height="100vh" />
@@ -43,9 +48,12 @@ export const getServerSideProps = async (context: GetServerSidePropsContext): Pr
     };
   }
 
+  const pdbString = await getPeptidePdbContent(peptide.id);
+
   return {
     props: {
-      peptide
+      peptide,
+      pdbString
     }
   };
 };
