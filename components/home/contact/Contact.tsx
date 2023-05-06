020aab6d096af69cdc82bf6b85d8c979d214d311
BrokenRoto <superjrcm@gmail.com>
2023-05-05 22:29:53 -0500

    Contact members information changes

--- components/home/contact/Contact.tsx
+++ components/home/contact/Contact.tsx
@@ -12,28 +12,24 @@ const Contact: React.FC = () => {
   return (
     <div className={styles.contact} id="contact">
       <Content size="medium" textAlign="center" className={styles['header-container']}>
-        <h2 className={styles.header}>Meet our Team</h2>
+        <h2 className={styles.header}>Project Leader</h2>
       </Content>
       <div className={styles.members}>
-        <Content size="medium" textAlign="center">
-          <h3 className={styles['head-of-project-header']}>Head of Project</h3>
-        </Content>
         <Columns centered className={styles['head-project-columns']}>
           <Columns.Column size="half">
             <MemberCard
               className={styles['head-project-card']}
               name="Yovani Marrero-Ponce"
-              email="ymarrero77@yahoo.es"
+              email="ymarrero@usfq.edu.ec"
               shortCV="Yovani Marrero-Ponce received the BSc degree in Pharmaceutical Sciences (summa cum laude) from the Universidad Central de las Villas (UCLV), Santa Clara, Cuba, in 2001, the MSc degree in Biochemistry from Medical University “Dr. Serafin Ruiz-de Zarate Ruiz”, Santa Clara, Cuba, in 2004, and the PhD degree in Chemistry from Havana University, Havana City, Cuba, in 2005. After post-doctoral fellowships at the University of Valencia, Spain, he founded the Unit of Computer-Aided Molecular “Biosilico” Discovery and Bioinformatic Research (CAMD-BIR Unit, today is known as CAMD-BIR International Network) as a spin-off of the Department of Pharmacy at UCLV. His research interests include molecular modelling and drug discovery, chem-bio-med-informatics, molecular descriptor, chemogenomics, and mathematical, theoretical, and computational chemistry. Recently, Marrero-Ponce have been selected in the TOP 1% of researchers worldwide who have carried out research of excellence in the last 60 years in all fields of science. DOI: 10.17632/btchxktzyw.3."
               avatar={imageYovani.src}
-              phone="+593-2-297-1700"
               occupation={
                 <Fragment>
+                  <p>Professor-Researcher</p>
                   <p>Grupo de Medicina Molecular y Traslacional (MeM&T)</p>
                   <p>Escuela de Medicina.</p>
                   <p>Universidad San Francisco de Quito (USFQ)</p>
                   <p>Quito, 170157, Pichincha, Ecuador.</p>
-                  <p>ORCID: orcid.org/0000-0003-2721-1142</p>
                 </Fragment>
               }
               website="https://www.usfq.edu.ec/es/perfil/yovani-marrero-ponce "
@@ -42,15 +38,17 @@ const Contact: React.FC = () => {
           <Columns.Column size="half">
             <MemberCard
               className={styles['head-project-card']}
-              name="Noel Pérez Pérez"
+              name="Noel Pérez-Pérez"
               email="nperez@usfq.edu.ec"
               shortCV="Noel Pérez-Pérez (M'19-SM'23) received a B.Eng. degree in computer science engineering and a M.Sc. degree in applied computer science in the field of digital image processing, pattern recognition, and machine learning from the Universidad de Ciego de Avila, Cuba, in 2005 and 2007, respectively, and the Ph.D. degree in the field of data mining, from the University of Porto, Portugal, in 2015. From 2008 to 2015, he was a Fellow Researcher with the Institute of Mechanical Engineering and Industrial Management, Faculty of Engineering, University of Porto. From 2015 to 2017, he was with the Instituto de Telecomunicações, Faculty of Sciences, University of Porto, as a Postdoctoral Fellow. Since 2017, he has been a Full-Time Professor at the Universidad San Francisco de Quito, Ecuador, where he co-founded the Applied Signal Processing and Machine Learning Research Group. His research interests include digital image processing, data mining, pattern recognition, and machine learning."
               avatar={imageNoel.src}
               occupation={
                 <Fragment>
-                  <p>Proffessor-Researcher</p>
+                  <p>Professor-Researcher</p>
                   <p>Applied Signal Processing & Machine Learning (ASP&ML) research group</p>
-                  <p>Universidad San Francisco de Quito</p>
+                  <p>Colegio Politécnico</p>
+                  <p>Universidad San Francisco de Quito (USFQ)</p>
+                  <p>Quito, 170157, Pichincha, Ecuador.</p>
                 </Fragment>
               }
               website="https://www.usfq.edu.ec/es/perfiles/noel-perez-perez"
@@ -64,6 +62,13 @@ const Contact: React.FC = () => {
           <Columns.Column size="one-third">
             <MemberCard
               name="Longendri Aguilera Mendoza"
+              occupation={
+                <Fragment>
+                  <p>Researcher</p>
+                  <p>Centro de Investigación Científica y de Educación Superior de Ensenada (CICESE)</p>
+                  <p>Baja California, México</p>
+                </Fragment>
+              }
               email="longendri@gmail.com"
               shortCV="PhD in computer science, Center for Scientific Research and Higher Education of Ensenada (CICESE), Ensenada, Mexico. Masters in bioinformatics, Higher Institute of Technologies and Applied Sciences (InSTEC), Havana, Cuba. Bachelor of Computer Science, University of Havana, Havana, Cuba. An enthusiast Data Engineer/Scientist and Software Developer with a background in using technology to get insight into a large amount of data."
               avatar={imageLongendri.src}
@@ -79,6 +84,8 @@ const Contact: React.FC = () => {
                 <Fragment>
                   <p>Senior Researcher.</p>
                   <p>Applied Physics Division</p>
+                  <p>Centro de Investigación Científica y de Educación Superior de Ensenada (CICESE)</p>
+                  <p>Baja California, México</p>
                 </Fragment>
               }
             />
@@ -88,6 +95,13 @@ const Contact: React.FC = () => {
               name="Cesar Jaca"
               email="cesar@email.com"
               shortCV="Short CV Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim ante sit amet quam dictum, pellentesque mattis arcu ornare. Morbi et mauris posuere"
+              occupation={
+                <Fragment>
+                  <p>Cátedras CONACYT (research SNI I)</p>
+                  <p>Centro de Investigación Científica y de Educación Superior de Ensenada (CICESE)</p>
+                  <p>Baja California, México</p>
+                </Fragment>
+              }
               avatar={imageEmpty.src}
             />
           </Columns.Column>
