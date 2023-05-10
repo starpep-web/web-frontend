666b77c8d583ff2833063f1b2e86b2b483c7a733
moonstar-x <christian.lopez99@outlook.com>
2023-05-10 01:12:13 -0500

    Renamed datamining api config to python rest api.

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@
 export const NODE_ENV = process.env.NODE_ENV as string;
 export const BUILDING_STAGE = process.env.BUILDING_STAGE as string;
 
-export const DATAMINING_API_URL = process.env.DATAMINING_API_URL as string;
+export const PYTHON_REST_API_URL = process.env.PYTHON_REST_API_URL as string;
 export const NEO4J_DB_URI = process.env.NEO4J_DB_URI as string;
 export const SERVER_DOWNLOADS_URL = process.env.SERVER_DOWNLOADS_URL as string;
 
