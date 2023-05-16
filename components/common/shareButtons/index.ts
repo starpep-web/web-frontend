1471ae7b83ec85caf4c7ab733f79285fbcc4588d
moonstar-x <christian.lopez99@outlook.com>
2023-05-16 11:49:51 -0500

    Created a ShareButtons component.

---
+++ components/common/shareButtons/index.ts
@@ -0,0 +1,15 @@
+import ShareButtons from './ShareButtons';
+
+import FacebookShareButton from './buttons/FacebookShareButton';
+import TwitterShareButton from './buttons/TwitterShareButton';
+import LinkedInButton from './buttons/LinkedInButton';
+import CopyToClipboardShareButton from './buttons/CopyToClipboardShareButton';
+
+export {
+  ShareButtons,
+
+  FacebookShareButton,
+  TwitterShareButton,
+  LinkedInButton,
+  CopyToClipboardShareButton
+};
