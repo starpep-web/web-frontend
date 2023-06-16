77445308d73bff97ad6839b21b18208f3067b5e1
moonstar-x <christian.lopez99@outlook.com>
2023-06-16 15:04:16 -0500

    Partially implemented single query peptide search box.

--- components/search/peptideSearchBox/SingleQueryPeptideSearchBox.tsx
+++ components/search/peptideSearchBox/SingleQueryPeptideSearchBox.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
-import { ComingSoonPlaceholder } from '@components/common/comingSoon';
+import React, { useState } from 'react';
+import { Form, Button, Icon } from 'react-bulma-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Dropdown } from '@components/form/dropdown';
+
+const queryPlaceholder = '>Query\nGIGAVLKVLTTGLPALISWIKRKRQQ';
 
 const SingleQueryPeptideSearchBox = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleOnSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+    console.log('submit');
+  };
+
   return (
-    <ComingSoonPlaceholder />
+    <form onSubmit={handleOnSubmit}>
+      <Form.Field>
+        <Form.Label>
+          Single Query in FASTA Format
+        </Form.Label>
+
+        <Form.Control>
+          <Form.Textarea fixedSize placeholder={queryPlaceholder} />
+        </Form.Control>
+      </Form.Field>
+
+      <hr />
+
+      <Form.Label>
+        Alignment Options
+      </Form.Label>
+
+      <Dropdown
+        label="Substitution Matrix"
+        options={['']}
+        value=""
+        onChange={() => null}
+      />
+
+      <Button.Group align="center">
+        <Button color="primary" loading={loading} disabled={loading}>
+          <Icon align="left">
+            <FontAwesomeIcon icon="search" />
+          </Icon>
+
+          <span>
+            Search
+          </span>
+        </Button>
+      </Button.Group>
+    </form>
   );
 };
 
