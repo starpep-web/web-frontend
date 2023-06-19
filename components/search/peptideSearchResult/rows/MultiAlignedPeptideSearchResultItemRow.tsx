d2ded2cac82f554e05595aeaafb77c93d50710da
moonstar-x <christian.lopez99@outlook.com>
2023-06-18 22:28:22 -0500

    Implemented multi query page.

---
+++ components/search/peptideSearchResult/rows/MultiAlignedPeptideSearchResultItemRow.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Link from 'next/link';
+import { DYNAMIC_ROUTES } from '@lib/constants/routes';
+import { MultiAlignedPeptide } from '@lib/models/search';
+import { RowProps } from './types';
+
+const MultiAlignedPeptideSearchResultItemRow: React.FC<RowProps<MultiAlignedPeptide>> = ({
+  index,
+  id,
+  sequence,
+  length,
+  score,
+  avg_score,
+  min_score,
+  max_score
+}) => {
+  return (
+    <tr>
+      <th>
+        {index}
+      </th>
+      <td>
+        {id}
+      </td>
+      <td>
+        {score.toLocaleString('en-US', {
+          maximumFractionDigits: 2,
+          minimumFractionDigits: 2
+        })}
+      </td>
+      <td>
+        {avg_score.toLocaleString('en-US', {
+          maximumFractionDigits: 2,
+          minimumFractionDigits: 2
+        })}
+      </td>
+      <td>
+        {max_score.toLocaleString('en-US', {
+          maximumFractionDigits: 2,
+          minimumFractionDigits: 2
+        })}
+      </td>
+      <td>
+        {min_score.toLocaleString('en-US', {
+          maximumFractionDigits: 2,
+          minimumFractionDigits: 2
+        })}
+      </td>
+      <td>
+        <Link href={DYNAMIC_ROUTES.peptide(sequence)}>
+          {sequence}
+        </Link>
+      </td>
+      <td>
+        {length}
+      </td>
+    </tr>
+  );
+};
+
+export default MultiAlignedPeptideSearchResultItemRow;
