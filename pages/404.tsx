6fb45fce912a36a4f4cb4d8a79134bda75714e57
moonstar-x <christian.lopez99@outlook.com>
2023-06-19 14:51:36 -0500

    Implemented 404 and 500 pages.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { PageMetadata } from '@components/common/pageMetadata';
 import { PageWrapper } from '@components/common/pageWrapper';
+import { AppErrorMessage } from '@components/common/appErrorMessage';
 
 const NotFoundPage = () => {
   return (
     <PageWrapper isErrorPage>
       <PageMetadata title="Not Found" />
 
-      Not Found :(
+      <AppErrorMessage
+        icon="question-circle"
+        iconColor="primary"
+        title="Page Not Found"
+        message="The page you were looking for was not found. Please check that the URL you inserted is correct."
+      />
     </PageWrapper>
   );
 };
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { PageMetadata } from '@components/common/pageMetadata';
 import { PageWrapper } from '@components/common/pageWrapper';
+import { AppErrorMessage } from '@components/common/appErrorMessage';
 
 const ServerSideErrorPage = () => {
   return (
     <PageWrapper isErrorPage>
       <PageMetadata title="Server Side Error" />
 
-      Server Side Error
+      <AppErrorMessage
+        icon="triangle-exclamation"
+        iconColor="danger"
+        title="Server Side Error"
+        message="The server has failed to fulfill your request because of an unknown error. Please try again later."
+      />
     </PageWrapper>
   );
 };
