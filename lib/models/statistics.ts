eb7af705a0666c3ce4fff9fa3041ee1e5933637b
moonstar-x <christian.lopez99@outlook.com>
2023-08-03 03:43:53 -0500

    Created histogram statistics service for features.

--- lib/models/statistics.ts
+++ lib/models/statistics.ts
@@ -1,23 +1,51 @@
 export interface PartialRelationStatistics {
-  distribution: Record<string, number>,
-  percentage: Record<string, number>,
-  total: number,
+  distribution: Record<string, number>
+  percentage: Record<string, number>
+  total: number
   partialSize: number
 }
 
 export interface DatabaseGeneralInformationStatistics {
-  count: number,
-  unusualCount: number,
-  functionDistribution: Record<string, number>,
+  count: number
+  unusualCount: number
+  functionDistribution: Record<string, number>
   databaseDistribution: Record<string, number>
 }
 
 export interface DatabaseMetadataStatistics {
-  lengthDistribution: Record<number, number>,
-  functionDistribution: Record<string, number>,
-  databaseDistribution: Record<string, number>,
-  targetDistribution: PartialRelationStatistics,
-  originDistribution: PartialRelationStatistics,
-  cTerminusDistribution: PartialRelationStatistics,
+  lengthDistribution: Record<number, number>
+  functionDistribution: Record<string, number>
+  databaseDistribution: Record<string, number>
+  targetDistribution: PartialRelationStatistics
+  originDistribution: PartialRelationStatistics
+  cTerminusDistribution: PartialRelationStatistics
   nTerminusDistribution: PartialRelationStatistics
+}
+
+export interface DatabaseFeaturesStatistics {
+  hydropathicityHistogram: Record<string, number>
+  chargeHistogram: Record<string, number>
+  isoelectricPointHistogram: Record<string, number>
+  bomanIndexHistogram: Record<string, number>
+  gaacAlphaticHistogram: Record<string, number>
+  gaacAromaticHistogram: Record<string, number>
+  gaacPostiveChargeHistogram: Record<string, number>
+  gaacNegativeChargeHistogram: Record<string, number>
+  gaacUnchargeHistogram: Record<string, number>
+  hydrophobicityHistogram: Record<string, number>
+  solvationHistogram: Record<string, number>
+  amphiphilicityHistogram: Record<string, number>
+  hydrophilicityHistogram: Record<string, number>
+}
+
+export type HistogramWidthMethod = 'scott' | 'freedman-diaconis';
+export interface HistogramData {
+  min: number
+  max: number
+  width: number
+  numOfBins: number
+  bins: {
+    classNum: number
+    frequency: number
+  }[]
 }
