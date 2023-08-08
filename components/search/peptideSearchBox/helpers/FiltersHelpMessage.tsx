2f468b4710c49398c34b98fa2123ae1e8ca3a969
moonstar-x <christian.lopez99@outlook.com>
2023-08-08 00:27:40 -0500

    Updated FiltersHelpMessage.

--- components/search/peptideSearchBox/helpers/FiltersHelpMessage.tsx
+++ components/search/peptideSearchBox/helpers/FiltersHelpMessage.tsx
@@ -7,7 +7,7 @@ const FiltersHelpMessage = () => {
       <Notification color="info">
         Click on the plus icon and select the filters you wish to use in your search.
         You can choose from a variety of operators and comparators for both metadata and features.
-        Each filter will be evaluated sequentially, <span className="is-underlined">there is no precedence of <strong>AND</strong> over <strong>OR</strong> operators.</span>
+        Each filter will be evaluated sequentially in the order that they're inserted, <span className="is-underlined">there is no precedence of <strong>AND</strong> over <strong>OR</strong> operators.</span>
       </Notification>
     </Block>
   );
