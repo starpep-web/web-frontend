9f1d82a7889adc0c3d6a9bfb38f01049cf76532b
moonstar-x <christian.lopez99@outlook.com>
2023-08-21 15:38:59 -0500

    Added component structure to export page.

--- pages/search/export/[type]/[id].tsx
+++ pages/search/export/[type]/[id].tsx
@@ -2,26 +2,68 @@ import React from 'react';
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import { PageMetadata } from '@components/common/pageMetadata';
 import { PageWrapper } from '@components/common/pageWrapper';
-import { SearchType, isSearchTypeValid } from '@lib/models/export';
+import { SearchExportLoadingNoProgress, SearchExportLoadingWithProgress } from '@components/searchExport/searchExportLoading';
+import { SearchExportError } from '@components/searchExport/searchExportError';
+import { SearchExportComplete } from '@components/searchExport/searchExportComplete';
+import { isSearchTypeValid, ExportResult } from '@lib/models/export';
+import { AsyncTaskResponse } from '@lib/services/pythonRestApi/apiService';
+import { getSearchExportResult } from '@lib/services/pythonRestApi/exportService';
 
 interface ServerSideProps {
-  type: SearchType
-  taskId: string
+  result: AsyncTaskResponse<ExportResult>
 }
 
 interface Props extends ServerSideProps {
 
 }
 
-const SearchExportPage: React.FC<Props> = ({ type, taskId }) => {
+const SearchExportPage: React.FC<Props> = ({ result }) => {
+  if (result.loading) {
+    return (
+      <PageWrapper>
+        <PageMetadata
+          title={`Loading... - Export Search - ${result.id}`}
+        />
+
+        {
+          result.data ? (
+            <SearchExportLoadingWithProgress
+              done={result.data.done}
+              form={result.data.form}
+              total={result.data.total}
+            />
+          ) : (
+            <SearchExportLoadingNoProgress />
+          )
+        }
+      </PageWrapper>
+    );
+  }
+
+  if (!result.success) {
+    return (
+      <PageWrapper>
+        <PageMetadata
+          title={`Error - Export Search - ${result.id}`}
+        />
+
+        <SearchExportError error={result.data} />
+      </PageWrapper>
+    );
+  }
+
   return (
     <PageWrapper>
-      <PageMetadata title={`Export Search | ${taskId}`} />
+      <PageMetadata
+        title={`Export Search - ${result.id}`}
+      />
 
-      <hr />
-      <div>
-        {type} - {taskId}
-      </div>
+      <SearchExportComplete
+        id={result.id}
+        form={result.data.form}
+        peptideIds={result.data.peptideIds}
+        total={result.data.total}
+      />
     </PageWrapper>
   );
 };
@@ -36,12 +78,23 @@ export const getServerSideProps = async (context: GetServerSidePropsContext): Pr
     };
   }
 
-  return {
-    props: {
-      type,
-      taskId
+  try {
+    const result = await getSearchExportResult(type, taskId);
+    if (!result.loading && !result.success) {
+      console.error(result);
     }
-  };
+
+    return {
+      props: {
+        result
+      }
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      notFound: true
+    };
+  }
 };
 
 export default SearchExportPage;
