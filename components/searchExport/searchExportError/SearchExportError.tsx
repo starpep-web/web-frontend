c819b17310c8c53b0f7c57f56b9b41b71da913f7
moonstar-x <christian.lopez99@outlook.com>
2023-08-21 16:09:28 -0500

    Implemented SearchExportError component.

--- components/searchExport/searchExportError/SearchExportError.tsx
+++ components/searchExport/searchExportError/SearchExportError.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { ErrorMessage } from '@components/common/errorMessage';
 
 interface Props {
   error: string
 }
 
-const SearchExportError: React.FC<Props> = () => {
-  return null;
+const SearchExportError: React.FC<Props> = ({ error }) => {
+  return (
+    <ErrorMessage
+      header="An error occurred when exporting your search"
+      description="The server could not complete your search export. If you see this message, please try exporting your search again."
+      error={error}
+      show
+    />
+  );
 };
 
 export default SearchExportError;
