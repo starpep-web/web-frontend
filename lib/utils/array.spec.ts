1b21e83dd0818a32131ef4f22ded8b8e64e80439
moonstar-x <christian.lopez99@outlook.com>
2023-08-21 19:44:20 -0500

    Created a joinWithDifferentLastSeparator array util.

--- lib/utils/array.spec.ts
+++ lib/utils/array.spec.ts
@@ -1,4 +1,4 @@
-import { createAlphabet, partitionArray } from './array';
+import { createAlphabet, partitionArray, joinWithDifferentLastSeparator } from './array';
 
 describe('Utils: Array', () => {
   describe('createAlphabet()', () => {
@@ -23,6 +23,31 @@ describe('Utils: Array', () => {
       ];
 
       expect(partitionArray(arr, 3)).toStrictEqual(expected);
+    });
+  });
+
+  describe('joinWithDifferentLastSeparator()', () => {
+    const separator = ', ';
+    const lastSeparator = ', and ';
+
+    it('should return an empty string if array has no items.', () => {
+      const actual = joinWithDifferentLastSeparator([], separator, lastSeparator);
+      expect(actual).toBe('');
+    });
+
+    it('should return a string with only last item if array has 1 item.', () => {
+      const actual = joinWithDifferentLastSeparator(['hi'], separator, lastSeparator);
+      expect(actual).toBe('hi');
+    });
+
+    it('should return a string with only last separator if array has 2 items.', () => {
+      const actual = joinWithDifferentLastSeparator(['hi', 'bye'], separator, lastSeparator);
+      expect(actual).toBe('hi, and bye');
+    });
+
+    it('should return a correctly joined string.', () => {
+      const actual = joinWithDifferentLastSeparator([1, 2, 3, 4], separator, lastSeparator);
+      expect(actual).toBe('1, 2, 3, and 4');
     });
   });
 });
--- lib/utils/array.ts
+++ lib/utils/array.ts
@@ -18,3 +18,17 @@ export const partitionArray = <T>(arr: T[], size: number): T[][] => {
 
   return partitioned;
 };
+
+export const joinWithDifferentLastSeparator = <T>(arr: T[], sep: string, lastSep: string): string => {
+  if (!arr.length) {
+    return '';
+  }
+
+  const lastItem = arr[arr.length - 1];
+  if (arr.length === 1) {
+    return `${lastItem}`;
+  }
+
+  const firstItems = arr.slice(0, arr.length - 1);
+  return `${firstItems.join(sep)}${lastSep}${lastItem}`;
+};
