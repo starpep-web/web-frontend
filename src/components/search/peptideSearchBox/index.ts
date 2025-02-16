ca0e589e06233b42b94d82b020e255e7c53a85ec
moonstar-x <christian.lopez99@outlook.com>
2025-02-16 01:48:21 -0500

    Added search box in single query results page.

--- src/components/search/peptideSearchBox/index.ts
+++ src/components/search/peptideSearchBox/index.ts
@@ -1,2 +1,4 @@
 export { PeptideSearchBox } from './PeptideSearchBox';
 export { WrappedTextQueryPeptideSearchBox } from './wrapped/wrappedTextQueryPeptideSearchBox';
+export { WrappedSingleQueryPeptideSearchBox } from './wrapped/wrappedSingleQueryPeptideSearchBox';
+export { WrappedMultiQueryPeptideSearchBox } from './wrapped/wrappedMultiQueryPeptideSearchBox';
